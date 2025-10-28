4e49c9c55d62771aaa11676d22bda09b8fcf0394
dkssud05 <hyeonuson51@gmail.com>
2025-10-28 17:44:20 +0900

    grass1 grass2 위치 설정

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ def handle_events():
 def reset_world():
     global boy
 
-    grass = Grass()
-    game_world.add_object(grass, 0)
+    grass1 = Grass(400, 10)
+    grass2 = Grass(400, 30)
+    game_world.add_object(grass1, 0)
     boy = Boy()
     game_world.add_object(boy, 1)
 
